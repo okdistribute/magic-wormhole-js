0522519328c21fdaf4f2015eb28cb64b0b634fbd
okdistribute <633012+okdistribute@users.noreply.github.com>
2021-04-18 12:25:27 -0700

    add web version of spake2 wasm

--- build-spake2-wasm.sh
+++ build-spake2-wasm.sh
@@ -3,6 +3,10 @@
 set -e
 
 cd spake2-wasm
-wasm-pack build --target=nodejs
+wasm-pack build --target=nodejs --out-dir=pkg
 rm pkg/.gitignore pkg/package.json pkg/README.md
+
+wasm-pack build --target=web --out-dir=web
+rm web/.gitignore web/package.json web/README.md
+
 cd ..
